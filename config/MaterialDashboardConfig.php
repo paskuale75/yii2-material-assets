10a479775c8c47dfaea24491a36754b4c8928dff
paskuale <pellicanipasquale@gmail.com>
2019-10-13 14:16:28 +0200

    first commit

---
+++ config/MaterialDashboardConfig.php
@@ -0,0 +1,40 @@
+<?php
+namespace paskuale75\config;
+
+use Yii;
+
+class MaterialDashboardConfig
+{
+    public static function sidebarColor()
+    {
+        /** @var MaterialDashboardAsset $bundle */
+        $bundle = Yii::$app->assetManager->getBundle('spl\web\MaterialDashboardAsset');
+
+        return $bundle->sidebarColor;
+    }
+
+
+    public static function sidebarBackgroundColor()
+    {
+        /** @var MaterialDashboardAsset */
+        $bundle = Yii::$app->assetManager->getBundle('spl\web\MaterialDashboardAsset');
+
+        return $bundle->sidebarBackgroundColor;
+    }
+
+    public static function sidebarBackgroundImage()
+    {
+        /** @var MaterialDashboardAsset */
+        $bundle = Yii::$app->assetManager->getBundle('spl\web\MaterialDashboardAsset');
+
+        return $bundle->sidebarBackgroundImage;
+    }
+    
+    public static function siteTitle()
+    {
+        /** @var MaterialDashboardAsset */
+        $bundle = Yii::$app->assetManager->getBundle('spl\web\MaterialDashboardAsset');
+
+        return $bundle->siteTitle;
+    }
+}
